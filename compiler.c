dea84154e21ebf2e337c68e58b6722d35cb924ab
LIHIHAIM <62651206+LIHIHAIM@users.noreply.github.com>
2021-02-18 20:55:56 +0200

    Add files via upload

--- compiler.c
+++ compiler.c
@@ -16,7 +16,7 @@ boolean executeCom(char *);
 char* dirCommands[DIR_COMS] =  {".data", ".string", ".entry", ".extern"}; /* the directive commands */
 enum cmds {data, string, entry, external};
 
-IC = 100, DC = 0, lineInd = 0;
+IC = 100, DC = 0, lineInd = 0, IS_LABLE=0, NEXT_WORD=0, ERROR=0;
 
 /* compile(): The function compiles an Assembly file and make one to three files in addition:
  file.ob - a binary file, which includes the memory....
@@ -25,7 +25,86 @@ IC = 100, DC = 0, lineInd = 0;
  file.ext - an "extern" file, which includes the names of all the external variables (from other files) 
             and their calling address in the RAM.
 */
-boolean compile(char *fileName){
+boolean second_compile(char *fileName){
+     FILE *fd = openf(fileName,"r");
+     char *tempLine;
+     senType type;
+     boolean symbol = FALSE;
+
+     compSuc = TRUE;
+
+     if(!fd)
+          return FALSE;
+     while(FOREVER){ 
+          char *optLabel = calloc(LABEL_SIZE,sizeof(char));
+          if(!isAlloc(optLabel))
+               return FALSE;
+          tempLine = readLine(fd,LINE);
+          if(*tempLine == '\0')
+              break;
+          strcpy(line,tempLine);
+          free(tempLine);
+
+          if(getOptLabel(optLabel)){
+               compSuc = TRUE;
+               continue;
+          }
+          else
+          {
+               IS_LABLE=TRUE;
+          }
+          switch(type){
+               case blank:
+               case comment:
+               break;
+               case directive:
+                    int command = getDirCom(command);
+                    switch(command){
+                         case data:
+                         case string:
+                              char **params;
+                              if(symbol == TRUE){
+                                   if(/* search(optLabel) */){
+                                        /* some error */
+                                        compSuc = FALSE;
+                                        continue;
+                                   }
+                                   /*push(".data", DC, optLabel)*/
+                                   DC++;
+                              }
+                              params = getParams();
+                              pushParams(params, DC);
+                              continue;
+                         case external:
+                              continue;
+                         case entry:
+                              /*push(".data", DC, optLabel)*/ 
+                                   DC++;
+                                   if(/* search(optLabel) */){
+                                        compSuc = FALSE;
+                                        continue;
+                                   }
+                                   else
+                                   {
+                                        /* some error */
+                                        continue;
+                                   }
+                         case default:
+                              /*push(".data", DC, optLabel)*/ 
+                              if(/* search(optLabel) */){
+                                        /* some error */
+                                        compSuc = FALSE;
+                                        continue;
+                                   }
+                              continue;
+                    }
+               break;
+           
+     }
+
+}
+ 
+ boolean compile(char *fileName){
      FILE *fd = openf(fileName,"r");
      char *tempLine;
      senType type;
@@ -115,9 +194,10 @@ boolean compile(char *fileName){
           }
      }
 
-     /* */
+     /*Can we delete this loop? */
      while(FOREVER){}
 }
+
      /*while(FOREVER){
           tempLine = readLine(fd,LINE);
           if(*tempLine == '\0')
